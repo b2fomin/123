6ca71117bc411efbf17ee306a1ca5f39cfbd1798
gr006 <fomin.nv@phystech.edu>
2021-03-15 16:22:49 +0300

    changes in line_up

--- import.py
+++ import.py
@@ -3,21 +3,18 @@ import time
 
 PINS=[24,25,8,7,12,16,20,21]
 GPIO.setmode(GPIO.BCM)
-def line_up(led_number,period):
-    GPIO.output(led_number,True)
+def line_up_or_down(led_number,period,state):
+    GPIO.output(led_number,state)
     time.sleep(period)
-    GPIO.output(led_number,False)
+    GPIO.output(led_number,not state)
 
 def blink(led_number,blink_count,blink_period):
     GPIO.setup(led_number,GPIO.OUT)
     for _ in range(blink_count):
-        line_up(led_number,blink_period)
+        line_up(led_number,blink_period,True)
         time.sleep(blink_period)
 
 def running_light(count,period):
     GPIO.setup(PINS,False)
     for number in range(count):
-        line_up(PINS[number%len(PINS)],period)
-
-
-running_light(4,0.5)
+        line_up(PINS[number%len(PINS)],period,True)
