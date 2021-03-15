084a35eb32b9d607b5a3b9c58837972b6ee09c2c
gr006 <fomin.nv@phystech.edu>
2021-03-15 16:20:41 +0300

    running_light

--- import.py
+++ import.py
@@ -3,7 +3,6 @@ import time
 
 PINS=[24,25,8,7,12,16,20,21]
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PINS,GPIO.OUT)
 def line_up(led_number,period):
     GPIO.output(led_number,True)
     time.sleep(period)
@@ -14,3 +13,11 @@ def blink(led_number,blink_count,blink_period):
     for _ in range(blink_count):
         line_up(led_number,blink_period)
         time.sleep(blink_period)
+
+def running_light(count,period):
+    GPIO.setup(PINS,False)
+    for number in range(count):
+        line_up(PINS[number%len(PINS)],period)
+
+
+running_light(4,0.5)
