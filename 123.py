1b574d0e944f94915234a1c5895e8b5588edbb41
Юрко Ксения <gorbunov.ei@phystech.edu>
2021-03-29 19:02:35 +0300

    123

--- 123.py
+++ 123.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 PINS=[10,9,11,5,6,13,19,26]
 GPIO.setmode(GPIO.BCM)
@@ -19,14 +21,37 @@ def light_number(number,_time):
     for i in range(len(A)):
         if A[i]==1:
             GPIO.output(PINS[i],True)
-            time.sleep(_time)
+    time.sleep(_time)
 
 def repeat(repetitionsNumber,frequency):
-    for i in range(repetitionsNumber):
+    for i in range(repetitionsNumber//2):
         for number in range(256):
-            light_number(number,round(256/frequency,1))
+            light_number(number,float(256)/frequency)
+        for number in range(255,-1,-1):
+            light_number(number,float(256)/frequency)
+
+def make_array(frequency):
+    A=[]
+    i=0
+    while i <=1:
+        A.append(int(256*np.sin(i)))
+        i+=float(2*np.pi)/frequency
+    plt.plot(np.array(A),np.sin(np.array(A)))
+    plt.show()
+    return A
+
+def sinus(_time,frequency):
+    A=make_array(frequency)
+    for _ in range(_time//2):
+        for j in A:
+            light_number(j,float(0.5)/len(A))
+        for j in reversed(A):
+            light_number(j,float(0.5)/len(A))
 
 if __name__=='__main__':
-    repetition=int(input('Enter number: '))
-    repeat(repetition,256*2)
+    try:
+        repeatnumber=abs(int(input('Enter number: ')))
+        sinus(repeatnumber,10000)
+    except:
+        pass
     GPIO.cleanup()
